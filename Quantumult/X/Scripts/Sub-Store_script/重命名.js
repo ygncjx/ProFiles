ef2e58f24d18a328ccad233af12b5f45fafcc8ca
The One ҉ <603728573@qq.com>
2021-03-21 20:02:18 +0800

    .

--- Quantumult/X/Scripts/Sub-Store_script/重命名.js
+++
@@ -1,8 +0,0 @@
-function operator(proxies){
-  const counter = {};
-  return proxies.map(p =>{
-    if(!counter[p.name])counter[p.name] = 0;
-    p.name = `${p.name} ${++counter[p.name]}`;
-    return p;
-  })
-}
